3f1534115a4525b366cdaa360619e74ffb095393
franns008 <franns008@gmail.com>
2025-10-05 23:42:54 -0300

    Se agrega TP3 Ej 1

---
+++ TP3/grpc-hello-server/src/main/java/pdytr/example/grpc/Client.java
@@ -0,0 +1,38 @@
+package pdytr.example.grpc;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class Client
+{
+    public static void main( String[] args ) throws Exception
+    {
+      // Channel is the abstraction to connect to a service endpoint
+      // Let's use plaintext communication because we don't have certs
+      final ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+        .usePlaintext(true)
+        .build();
+
+      // It is up to the client to determine whether to block the call
+      // Here we create a blocking stub, but an async stub,
+      // or an async stub with Future are always possible.
+      GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+      
+      
+      GreetingServiceOuterClass.HelloRequest request =
+        GreetingServiceOuterClass.HelloRequest.newBuilder()
+          .setName("Ray")
+          .build();
+
+
+      // Finally, make the call using the stub
+      GreetingServiceOuterClass.HelloResponse response =
+        stub.withDeadlineAfter(5, TimeUnit.SECONDS).greeting(request);
+
+      System.out.println(response);
+
+      // A Channel should be shutdown before stopping the process.
+      channel.shutdownNow();
+    }
+}
